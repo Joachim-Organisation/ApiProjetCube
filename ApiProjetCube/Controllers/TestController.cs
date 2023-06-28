799493d055732bfd3ec5daa15915789578549305
jguitton <joachim.guitton@gmail.com>
2023-06-28 11:42:12 +0200

    Mise en place d'un controller permettant de faire un test d'ingration avec des messagesForum fictifs.

---
+++ ApiProjetCube/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using ApiProjetCube.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiProjetCube.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MessageForum>>> GetTest()
+        {
+            var messageA = new MessageForum { Id = 1, Content = "Test A", DateCreation = DateTimeOffset.Now, IdSubjectForum = 0, IdUtilisateur = 0 };
+
+            var messageB = new MessageForum { Id = 2, Content = "Test B", DateCreation = DateTimeOffset.Now, IdSubjectForum = 1, IdUtilisateur = 1 };
+
+            var list = new List<MessageForum> { messageA, messageB };
+
+            return list;
+        }
+    }
+}
